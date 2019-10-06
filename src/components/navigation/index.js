a5ed27271d388ff9b31e1619bfa23778c1639ea5
Emelie Sidesiö <esidesio@gmail.com>
2019-10-06 16:47:05 +0200

    hamburger menu

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -5,24 +5,49 @@ import "./index.scss";
 class Navigation extends React.Component {
   render() {
     return (
-      <div
-        className={
-          this.props.location.pathname === "/testimonials"
-            ? "navContainer black"
-            : "navContainer white"
-        }
-      >
-        <div>
+      <div>
+        <div
+          className={
+            this.props.location.pathname === "/testimonials"
+              ? "navDesktop black"
+              : "navDesktop white"
+          }
+        >
           <Link to="/" className="navLeft">
             Francesca Romana Correale
           </Link>
+          <div className="navRight">
+            <a href="/#projects">Projects</a>
+            <Link to="/contact" className="navLeft">
+              Contact
+            </Link>
+            <a href="/testimonials">Testimonials</a>
+          </div>
         </div>
-        <div className="navRight">
-          <a href="/#projects">Projects</a>
-          <Link to="/contact" className="navLeft">
-            Contact
-          </Link>
-          <a href="/testimonials">Testimonials</a>
+
+        <input id="hamburger" type="checkbox" className="mobileCheckbox" />
+
+        <label className="hamburger" htmlFor="hamburger">
+          <div className="bar bar1"></div>
+          <div className="bar bar2"></div>
+          <div className="bar bar3"></div>
+        </label>
+
+        <div className="menu">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/#projects">Projects</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <a href="/testimonials">Testimonials</a>
+            </li>
+          </ul>
         </div>
       </div>
     );
